cd66177f98e7cf440cb1d949769dae73ea406338
Vladimir <LesnickBear@gmail.com>
2019-03-01 16:03:53 +0200

    Links replaced

--- lib/bloc/video_bloc.dart
+++ lib/bloc/video_bloc.dart
@@ -51,67 +51,104 @@ class VideoBloc {
       return [
         Video(
             url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443230~acl=%2A%2F924662021.mp4%2A~hmac=d21593e65a2970dcfc8056770b109fa90690862b86cb49674a252ebbfeacab0d/vimeo-prod-skyfire-std-us/01/730/10/253653841/924662021.mp4",
+                "https://player.vimeo.com/external/253653841.hd.mp4?s=60190a033ce007bfec8c78842658322eb65d9cb7&profile_id=175&oauth2_token_id=57447761",
             name: "Baskectball game",
             pictureURL:
                 "https://static-videos.pexels.com/videos/857790/free-video-857790.jpg"),
         Video(
-            url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443921~acl=%2A%2F1163932066.mp4%2A~hmac=03dae5ced83fdf8395dfae7e882381ac8ae6d5b43ff66f11314ce0442c12b3bd/vimeo-prod-skyfire-std-us/01/768/12/303843089/1163932066.mp4",
+            url: "https://player.vimeo.com/external/198581686.hd.mp4?s=4c12db8025496b7138c44d683e65307a5aa0c477&profile_id=119&oauth2_token_id=57447761",
             name: "Wind power",
             pictureURL:
                 "https://static-videos.pexels.com/videos/1649787/free-video-1649787.jpg"),
         Video(
             url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443979~acl=%2A%2F1163940106.mp4%2A~hmac=8c4861d1b5e104c59a5dbba20a4be6d027ed26a478a2378de45f29ce714751df/vimeo-prod-skyfire-std-us/01/768/12/303844889/1163940106.mp4",
+                "https://player.vimeo.com/external/303844889.hd.mp4?s=0931f8dedacd8d20e248e6074912567e260190a4&profile_id=172&oauth2_token_id=57447761",
             name: "Field",
             pictureURL:
                 "https://static-videos.pexels.com/videos/1649831/free-video-1649831.jpg"),
         Video(
             url:
                 "http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4",
-            name: "Fat bunny sample"),  Video(
+            name: "Fat bunny sample"),         Video(
             url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443230~acl=%2A%2F924662021.mp4%2A~hmac=d21593e65a2970dcfc8056770b109fa90690862b86cb49674a252ebbfeacab0d/vimeo-prod-skyfire-std-us/01/730/10/253653841/924662021.mp4",
+                "https://player.vimeo.com/external/253653841.hd.mp4?s=60190a033ce007bfec8c78842658322eb65d9cb7&profile_id=175&oauth2_token_id=57447761",
             name: "Baskectball game",
             pictureURL:
                 "https://static-videos.pexels.com/videos/857790/free-video-857790.jpg"),
         Video(
-            url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443921~acl=%2A%2F1163932066.mp4%2A~hmac=03dae5ced83fdf8395dfae7e882381ac8ae6d5b43ff66f11314ce0442c12b3bd/vimeo-prod-skyfire-std-us/01/768/12/303843089/1163932066.mp4",
+            url: "https://player.vimeo.com/external/198581686.hd.mp4?s=4c12db8025496b7138c44d683e65307a5aa0c477&profile_id=119&oauth2_token_id=57447761",
             name: "Wind power",
             pictureURL:
                 "https://static-videos.pexels.com/videos/1649787/free-video-1649787.jpg"),
         Video(
             url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443979~acl=%2A%2F1163940106.mp4%2A~hmac=8c4861d1b5e104c59a5dbba20a4be6d027ed26a478a2378de45f29ce714751df/vimeo-prod-skyfire-std-us/01/768/12/303844889/1163940106.mp4",
+                "https://player.vimeo.com/external/303844889.hd.mp4?s=0931f8dedacd8d20e248e6074912567e260190a4&profile_id=172&oauth2_token_id=57447761",
             name: "Field",
             pictureURL:
                 "https://static-videos.pexels.com/videos/1649831/free-video-1649831.jpg"),
         Video(
             url:
                 "http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4",
-            name: "Fat bunny sample"),  Video(
+            name: "Fat bunny sample"),         Video(
             url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443230~acl=%2A%2F924662021.mp4%2A~hmac=d21593e65a2970dcfc8056770b109fa90690862b86cb49674a252ebbfeacab0d/vimeo-prod-skyfire-std-us/01/730/10/253653841/924662021.mp4",
+                "https://player.vimeo.com/external/253653841.hd.mp4?s=60190a033ce007bfec8c78842658322eb65d9cb7&profile_id=175&oauth2_token_id=57447761",
             name: "Baskectball game",
             pictureURL:
                 "https://static-videos.pexels.com/videos/857790/free-video-857790.jpg"),
         Video(
-            url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443921~acl=%2A%2F1163932066.mp4%2A~hmac=03dae5ced83fdf8395dfae7e882381ac8ae6d5b43ff66f11314ce0442c12b3bd/vimeo-prod-skyfire-std-us/01/768/12/303843089/1163932066.mp4",
+            url: "https://player.vimeo.com/external/198581686.hd.mp4?s=4c12db8025496b7138c44d683e65307a5aa0c477&profile_id=119&oauth2_token_id=57447761",
             name: "Wind power",
             pictureURL:
                 "https://static-videos.pexels.com/videos/1649787/free-video-1649787.jpg"),
         Video(
             url:
-                "https://gcs-vimeo.akamaized.net/exp=1551443979~acl=%2A%2F1163940106.mp4%2A~hmac=8c4861d1b5e104c59a5dbba20a4be6d027ed26a478a2378de45f29ce714751df/vimeo-prod-skyfire-std-us/01/768/12/303844889/1163940106.mp4",
+                "https://player.vimeo.com/external/303844889.hd.mp4?s=0931f8dedacd8d20e248e6074912567e260190a4&profile_id=172&oauth2_token_id=57447761",
             name: "Field",
             pictureURL:
                 "https://static-videos.pexels.com/videos/1649831/free-video-1649831.jpg"),
         Video(
             url:
                 "http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4",
+            name: "Fat bunny sample"),         Video(
+            url:
+                "https://player.vimeo.com/external/253653841.hd.mp4?s=60190a033ce007bfec8c78842658322eb65d9cb7&profile_id=175&oauth2_token_id=57447761",
+            name: "Baskectball game",
+            pictureURL:
+                "https://static-videos.pexels.com/videos/857790/free-video-857790.jpg"),
+        Video(
+            url: "https://player.vimeo.com/external/198581686.hd.mp4?s=4c12db8025496b7138c44d683e65307a5aa0c477&profile_id=119&oauth2_token_id=57447761",
+            name: "Wind power",
+            pictureURL:
+                "https://static-videos.pexels.com/videos/1649787/free-video-1649787.jpg"),
+        Video(
+            url:
+                "https://player.vimeo.com/external/303844889.hd.mp4?s=0931f8dedacd8d20e248e6074912567e260190a4&profile_id=172&oauth2_token_id=57447761",
+            name: "Field",
+            pictureURL:
+                "https://static-videos.pexels.com/videos/1649831/free-video-1649831.jpg"),
+        Video(
+            url:
+                "http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4",
+            name: "Fat bunny sample"), Video(
+            url:
+            "https://player.vimeo.com/external/253653841.hd.mp4?s=60190a033ce007bfec8c78842658322eb65d9cb7&profile_id=175&oauth2_token_id=57447761",
+            name: "Baskectball game",
+            pictureURL:
+            "https://static-videos.pexels.com/videos/857790/free-video-857790.jpg"),
+        Video(
+            url: "https://player.vimeo.com/external/198581686.hd.mp4?s=4c12db8025496b7138c44d683e65307a5aa0c477&profile_id=119&oauth2_token_id=57447761",
+            name: "Wind power",
+            pictureURL:
+            "https://static-videos.pexels.com/videos/1649787/free-video-1649787.jpg"),
+        Video(
+            url:
+            "https://player.vimeo.com/external/303844889.hd.mp4?s=0931f8dedacd8d20e248e6074912567e260190a4&profile_id=172&oauth2_token_id=57447761",
+            name: "Field",
+            pictureURL:
+            "https://static-videos.pexels.com/videos/1649831/free-video-1649831.jpg"),
+        Video(
+            url:
+            "http://www.sample-videos.com/video123/mp4/720/big_buck_bunny_720p_20mb.mp4",
             name: "Fat bunny sample"),
       ];
     });
